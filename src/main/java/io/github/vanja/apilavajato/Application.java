2d7148a4369f66220d70ccdf4dcea2c073f9606e
Nosliejnav <nosliejnav@gmail.com>
2024-11-28 15:05:38 -0300

    Add estrutura e dependencias Spring Initializr

---
+++ src/main/java/io/github/vanja/apilavajato/Application.java
@@ -0,0 +1,13 @@
+package io.github.vanja.apilavajato;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApilavajatoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApilavajatoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/vanja/apilavajato/ApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.vanja.apilavajato;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApilavajatoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
